ec94716c2fe42fe5685f1208b907e6b6e3068588
saikeerthi555 <43855677+saikeerthi555@users.noreply.github.com>
2021-02-15 15:39:38 +0530

    Callback from the Note component to trigger a delete function.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,12 +12,23 @@ function App() {
       return [...prevNotes, newNote];
     });
   }
+
+  function deleteNote(id) {
+    //console.log("Delete was triggered");
+  }
+
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
       {notes.map((noteItem) => {
-        return <Note title={noteItem.title} content={noteItem.content} />;
+        return (
+          <Note
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
       })}
       {/* <Note key={1} title="Note title" content="Note content" /> //this is the static and above code is dynamic */}
       <Footer />
